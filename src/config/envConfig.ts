eeb29c83cf5a5eda14fe715427dab9f87d1461dd
sparshmahajan <sparshmahajan3@gmail.com>
2024-02-22 05:18:14 +0530

    fix: remove Coingecko api key check  from config

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -1,7 +1,4 @@
 export const envConfig = () => {
-  if (!process.env.COINGECKO_API_KEY) {
-    throw new Error("COINGECKO_API_KEY must be defined");
-  }
   if (!process.env.DB_URL) {
     throw new Error("DB_URL must be defined");
   }
